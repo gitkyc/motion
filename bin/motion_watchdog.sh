fbcdbdd4b4a5d1f6555e5d38459f9541cbe96d19
gitkyc <1075265540@qq.com>
2018-12-21 06:01:49 +0000

    add stop all action in watchdog

--- bin/motion_watchdog.sh
+++ bin/motion_watchdog.sh
@@ -7,8 +7,10 @@ do
     ps -aux | grep "python" | grep "motion.py"
     if [ $? -ne 0 ]
     then 
+        sleep 2s
+	$BIN_HOME/stop_all.sh
         echo "motion is not running, start motion ..."
-	sleep 1s
+	sleep 2s
         $BIN_HOME/start_all.sh
     else
 	echo "motion is running!"
